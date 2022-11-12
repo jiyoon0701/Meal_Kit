79c866960b3e46875a09827c7d0d2ae8f215816c
Kyoungmin1016 <84130518+Kyoungmin1016@users.noreply.github.com>
2022-11-13 00:12:12 +0900

    hotfix(완전고장)

--- kickit/src/main/java/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package dao;
-
-import dto.User;
-
-public interface UserDao {
-	
-	public void insert(User user);
-	public User selectOne(String email);
-}
--- kickit/src/main/java/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package service;
-
-import dto.User;
-
-public interface UserService {
-	public void userInsert(User user);
-	public User getUser(String email);
-}
